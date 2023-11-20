1a9eb7365e0eed555e2fa22720c24e30ef6f7f01
Diego Trujillo <diegotrujillo0210@gmail.com>
2023-11-20 18:57:39 -0500

    Add user model methods

--- database/models/user.Model.js
+++ database/models/user.Model.js
@@ -4,6 +4,7 @@ const db = require('../db');
 
 const User = {};
 
+// find user
 User.getAll = () => {
     const sql = `
         SELECT * FROM users
@@ -11,5 +12,43 @@ User.getAll = () => {
     return db.manyOrNone(sql);
 };
 
+// create users
+User.create = (user) => {
+    const sql = `
+        INSERT INTO
+            users(
+                name,
+                lastname,
+                email,  
+                password,
+                phone,
+                image
+            )
+        values($1,$2,$3,$4,$5,$6) RETURNING id
+        
+    `;
+    console.log("SQL <> ", user);
+    // console.log("SQL 32 <> ", sql);
+    return db.one(sql, [
+        user.name,
+        user.lastname,
+        user.email,
+        user.password,
+        user.phone,
+        user.image,
+    ]);
+};
+
+
 module.exports = User;
 
+// VALUES(
+//     $[name],
+//     $[lastname],
+//     $[email],
+//     $[password],
+//     $[phone],
+//     $[image],
+//     $[created_at],
+//     $[updated_at]
+// )
