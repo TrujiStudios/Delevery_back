ca45eb70fc15186db78b0f0fe9f3206027299321
Diego Trujillo <diegotrujillo0210@gmail.com>
2023-11-24 11:13:11 -0500

    Add role creation when creating a user

--- service/user.Service.js
+++ service/user.Service.js
@@ -1,4 +1,6 @@
 const User = require('../database/models/user.Model');
+const Role = require('../database/models/role.Model');
+// const {Role : roleModel} = require('../database/models/role.Model');
 
 
 class UserService {
@@ -9,6 +11,13 @@ class UserService {
     async getAllUsers(){
         const users = await this.user.getAll();
         return users;
+    }
+    //TODO : FALTA VALIDAR LOS DATOS DE REGISTRO DE USUARIO
+    async createUser(data){
+        const user = await this.user.create(data);
+        await Role.create(user.id, 1);
+        if(!user) throw new Error('Error al crear usuariooo');
+        return user;
     }
 }
 
