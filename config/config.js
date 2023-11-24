362a90d51a58d492b03a03bc6328b2386b467a4f
Diego Trujillo <diegotrujillo0210@gmail.com>
2023-11-24 09:18:58 -0500

    Refactor database configuration in config.js

--- config/config.js
+++ config/config.js
@@ -2,27 +2,21 @@ require("dotenv").config();
 
 
 const config = {
-    port: process.env.PORT || 3000,
-    env: process.env.NODE_ENV || 'development',
-    dbName: process.env.DB_NAME || "DB-XYZ",
-    dbUser: process.env.DB_USER || "root",
-    dbPassword: process.env.DB_PASSWORD || "root",
-    dbHos: process.env.DB_HOST || "127.0.0.1",
-    dbPort: process.env.DB_PORT || "3306",   
-    dbmongoUrl: process.env.DB_MONGOURL,
-    jwtSecret: process.env.JWT_SECRET,
-    emailService: process.env.EMAIL_SERVICE,
-    emailUser: process.env.EMAIL_USER,
-    emailPass: process.env.EMAIL_PASS,
-    secretOrKey: process.env.SECRET_OR_KEY,
-    database:{
-        dbUsername: process.env.DB_USERNAME,
-        dbPassword: process.env.DB_PASSWORD,
-        dbHost: process.env.DB_HOST,
-        dbName: process.env.DB_NAME,
-        // port: process.env.DB_PORT,
-      },
+  port: process.env.PORT || 3000,
+  env: process.env.NODE_ENV || 'development',
+  jwtSecret: process.env.JWT_SECRET,
+  emailService: process.env.EMAIL_SERVICE,
+  emailUser: process.env.EMAIL_USER,
+  emailPass: process.env.EMAIL_PASS,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  database: {
+    dbHost: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 5432,
+    dbName: process.env.DB_NAME || 'postgres',
+    dbUsername: process.env.DB_USERNAME || 'postgres',
+    dbPassword: process.env.DB_PASSWORD || 'admin',
+  },
 };
 
 
-module.exports = {config}
+module.exports = { config }
