9aceac702a810f8b7a997d1e67fccd5a33602fc8
Diego Trujillo <diegotrujillo0210@gmail.com>
2023-11-25 16:13:22 -0500

    Update configuration and server settings

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const {config} = require('../config/config')
 
 const app = require('..')
 
-app.listen(config.port, '192.168.0.7' || 'localhost', () => {
+app.listen(config.port, '192.168.0.6' || 'localhost', () => {
     console.log()
     console.log(`Example app listening at http://localhost:${config.port}`)
 })
