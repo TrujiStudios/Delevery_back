afc3ee5168ed79df09064468322204db3bfb801c
Diego Trujillo <diegotrujillo0210@gmail.com>
2023-11-28 13:40:10 -0500

    Add image upload functionality to user update

--- service/user.Service.js
+++ service/user.Service.js
@@ -1,5 +1,7 @@
 const User = require('../database/models/user.Model');
 const Role = require('../database/models/role.Model');
+const storage = require('../utils/cloud_storage');
+
 // const {Role : roleModel} = require('../database/models/role.Model');
 
 
@@ -19,6 +21,25 @@ class UserService {
         if(!user) throw new Error('Error al crear usuariooo');
         return user;
     }
+
+    async updateWithimages(user, files) {
+        if (files.length > 0) {
+            const pathImage = `images_${Date.now()}`
+            const url = await storage(files[0], pathImage, null)
+            if (url != undefined || url != null) {
+                user.image = url
+            }
+        }
+        console.log(user)
+        const results = await this.user.update(user);
+        return results;
+    }
+
+    async findByUserId(idUser){
+        const user = await this.user.findByUserId(idUser);
+        return user;
+    }
+    
 }
 
 module.exports = UserService;
