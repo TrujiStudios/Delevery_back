d558566c7b552f1467dde1952ab39a42af3414f5
Diego Trujillo <diegotrujillo0210@gmail.com>
2023-11-28 13:40:01 -0500

    Add image upload functionality and user search by ID

--- router/user.Router.js
+++ router/user.Router.js
@@ -9,7 +9,7 @@ const UserService = require("../service/user.Service");
 // const authMiddleware = require('../middleware/authValidation');
 
 
-function userRoute(app) {
+function userRoute(app,upload) {
     const router = express.Router();
     const userServ = new UserService()
 
@@ -31,6 +31,45 @@ function userRoute(app) {
                 err
             });
             // next(err);
+        }
+    });
+
+    router.get("/findByUserId/:idUser", async (req, res, next) => {
+        try {
+            const result = await userServ.findByUserId(req.params.idUser)
+            res.json({
+                success: true,
+                message: "get all",
+                data:result
+            })
+        } catch (err) {
+            console.log(`Error: ${err}`);
+            return res.status(501).json({
+                success: false,
+                message: "Error: al obtener los usuarios",
+                err
+            });
+            // next(err);
+        }
+    });
+
+    router.put("/update", upload.array('image', 1), async (req, res, next) => {
+        try {
+            const user = JSON.parse(req.body.user);
+            const files = req.files;
+            const result = await userServ.updateWithimages(user, files)
+            return res.status(201).json({
+                message: "Usuario actualizado con exito",
+                success: true,
+                error: null,
+            })
+        } catch (err) {
+            console.log(`Error: ${err}`);
+            return res.status(501).json({
+                message: "Error: al actualizar el usuario",
+                error: err.message,
+                success: false,
+            });
         }
     });
 
