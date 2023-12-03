962606f5690eb52ec049137e0d909d9c3d89ce98
Diego Trujillo <diegotrujillo0210@gmail.com>
2023-12-03 10:23:12 -0500

    Add passport authentication to create product route

--- router/product.Router.js
+++ router/product.Router.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const passport = require('passport');
+
 const ProductService = require("../service/product.Servise");
 
 
@@ -8,22 +10,24 @@ function productRoute(app) {
     const router = express.Router();
     const ProductServ = new ProductService()
 
-    app.use('/api/users', router);
+    app.use('/api/products', router);
 
-    router.post("/signup", async (req, res, next) => {
-        try {            
-            const result = await ProductServ.create(req.body)
+    router.post("/create",passport.authenticate('jwt', { session: false }), async (req, res, next) => {
+        try {         
+            const idUser = req.user.id;
+            const userId = req.body.userId; 
+            const result = await ProductServ.createCategory(req.body, idUser, userId);
             return res.status(201).json({
                 message: "Producto creado correctamente ",
                 success: true,
                 error: null,
                 data: result.id
             })
-        } catch (err) {
-            console.log(`Error: ${err}`);
+        } catch (error) {
+            console.error(`Error: ${error}`);
             return res.status(501).json({
-                message: "Error: al crear el producto",
-                error: err.message,
+                message: error.message,
+                error: error.message,
                 success: false,
             });
         }
