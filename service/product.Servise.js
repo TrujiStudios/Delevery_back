498bb955f631a5460f396001ca87c212f1a59692
Diego Trujillo <diegotrujillo0210@gmail.com>
2023-12-03 10:24:02 -0500

    Add user and category validation in createCategory method

--- service/product.Servise.js
+++ service/product.Servise.js
@@ -1,12 +1,21 @@
 const Product = require('../database/models/product.Model');
+const UserService = require('../service/user.Service');
+const categoryService = require('./Category.Service');
 
 class ProductService {
     constructor() {
         this.product = Product;
+        this.userServ = new UserService();
+        this.categoryServ = new categoryService();
+
     }
 
-    async create(product) {
-        const result = await this.product.create(product);
+    async createCategory(data, idUser, userId) {
+
+        await this.userServ.findByUserId(idUser, userId);
+        await this.categoryServ.findByCategoryId(data.category_id, idUser, userId);
+        
+        const result = await this.product.create(data);
         return result;
     }
 }
