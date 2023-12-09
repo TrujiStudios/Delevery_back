993a927560ab73b26be17e53f40acd5f55f84f56
Diego Trujillo <diegotrujillo0210@gmail.com>
2023-12-08 19:16:01 -0500

    Update server.js IP address

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const {config} = require('../config/config')
 
 const app = require('..')
 
-app.listen(config.port, '192.168.0.5' || 'localhost', () => {
+app.listen(config.port, '192.168.0.3' || 'localhost', () => {
     console.log()
     console.log(`Example app listening at http://localhost:${config.port}`)
 })
