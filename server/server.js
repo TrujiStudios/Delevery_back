889580069410e286ea4b8cd4dae7abb3b0a8a141
Diego Trujillo <diegotrujillo0210@gmail.com>
2023-12-14 10:33:47 -0500

    Update server.js to listen on a different IP address

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const {config} = require('../config/config')
 
 const app = require('..')
 
-app.listen(config.port, '192.168.0.3' || 'localhost', () => {
+app.listen(config.port, '192.168.0.9' || 'localhost', () => {
     console.log()
     console.log(`Example app listening at http://localhost:${config.port}`)
 })
