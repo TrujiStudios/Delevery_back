298973fc8b4ae8eb2247764adf0ff8685c7135a9
Diego Trujillo <diegotrujillo0210@gmail.com>
2023-12-14 12:27:23 -0500

    Update config.js with environment variables

--- config/config.js
+++ config/config.js
@@ -2,16 +2,16 @@ require("dotenv").config();
 
 
 const config = {
-  port: process.env.PORT,
-  env: process.env.NODE_ENV,
-  production: process.env.NODE_ENV,
+  port: process.env.PORT || 3000,
+  env: process.env.NODE_ENV || "development",
+  production: process.env.NODE_ENV === "production",
   emailService: process.env.EMAIL_SERVICE,
   emailUser: process.env.EMAIL_USER,
   emailPass: process.env.EMAIL_PASS,
-  secretOrKey: process.env.SECRET_OR_KEY,
-  id_developer: process.env.ID_DEVELOPER,
-  storageBucket: process.env.STORAGE_BUCKET,
-  sessionSecret: process.env.SESSION_SECERT,
+  secretOrKey: process.env.SECRET_OR_KEY || "2rwpcT6lN4lqEy72urwFOmPCldrleNx/5a7kAJD2jnXA7CjobsZLtY3uzWl4/YsKo21qGqDIhvx5de37tOc8mc/Wbr/waYQ2rnKbsgO+aywPSBMA1twGw3Xp4kftCUSI05rLtFiVfPYE3nDYZoekcBVFYbUZrB35xtAwusnOXHFFjKUUTdQZknC5n7hxcSdQz0GVaCfDPZ4rRkcIzrAOG0FEWERmW0+WEsBQbu/7X6fk2b4v1BcifpFZ5paClEcCHGmTbSoVC5fYTiyNU3/0BQ74+h2vgNERscTHUbwOYazeV8wssPhW4p1s1sWahwFbBk5cbANLCiAD2c9OXkC7EAKWpKnoRF4QPcfvdVowW0DbCXwqWwr0DLvDFYJ0fcc69Dc3WRAVUTGXC3NaTND/2hv+L5lgSdeQDLGv043xZsKaxLW4K5tOnVrJYRos6UT4rQZe3/PvgPb0paHVn/QoCRjjs8aQar6sC4jOMyJ+6nHsUeHNL3EEXkEAwUMlBdRLtmd0uFA1CzMUDohZ6uQ1eqQr8wuJnPyjfU6JSU4HDUPiD9UexzlVyvZlDZWDhm2reeLXiaSE12ClYDHm2VqyumiWuiEIqzdczgu3kpUVvGyzf4/IVRReoLAvyD0JEB/ChPTqE/h5lyicwaM/g+/PWS//aRbwQja4XZuHY13K/jw=",
+  id_developer: process.env.ID_DEVELOPER || "1",
+  storageBucket: process.env.STORAGE_BUCKET || "gs://delivey-app-trujistudios.appspot.com/",
+  sessionSecret: process.env.SESSION_SECERT || "2rwpcT6lN4lqEy72urwFOmPCldrleNx/5a7kAJD2jnXA7CjobsZLtY3uzWl4/YsKo21qGqDIhvx5de37tOc8mc/Wbr/waYQ2rnKbsgO+aywPSBMA1twGw3Xp4kftCUSI05rLtFiVfPYE3nDYZoekcBVFYbUZrB35xtAwusnOXHFFjKUUTdQZknC5n7hxcSdQz0GVaCfDPZ4rRkcIzrAOG0FEWERmW0+WEsBQbu/7X6fk2b4v1BcifpFZ5paClEcCHGmTbSoVC5fYTiyNU3/0BQ74+h2vgNERscTHUbwOYazeV8wssPhW4p1s1sWahwFbBk5cbANLCiAD2c9OXkC7EAKWpKnoRF4QPcfvdVowW0DbCXwqWwr0DLvDFYJ0fcc69Dc3WRAVUTGXC3NaTND/2hv+L5lgSdeQDLGv043xZsKaxLW4K5tOnVrJYRos6UT4rQZe3/PvgPb0paHVn/QoCRjjs8aQar6sC4jOMyJ+6nHsUeHNL3EEXkEAwUMlBdRLtmd0uFA1CzMUDohZ6uQ1eqQr8wuJnPyjfU6JSU4HDUPiD9UexzlVyvZlDZWDhm2reeLXiaSE12ClYDHm2VqyumiWuiEIqzdczgu3kpUVvGyzf4/IVRReoLAvyD0JEB/ChPTqE/h5lyicwaM/g+/PWS//aRbwQja4XZuHY13K/jw=",
   database: {
     dbHost: process.env.DB_HOST,
     port: process.env.DB_PORT,
