efc8c0e46a10dfc49795919d8834a998740fd114
Diego Trujillo <diegotrujillo0210@gmail.com>
2023-12-26 01:54:19 -0500

    Update server.js to listen on multiple IP addresses

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const {config} = require('../config/config')
 
 const app = require('..')
 
-app.listen(config.PORT, '192.168.0.9' || 'localhost', () => {
+app.listen(config.PORT, '192.168.0.9' || '172.30.217.10' || 'localhost', () => {
     console.log()
     console.log(`Example app listening at http://localhost:${config.PORT}`)
 })
